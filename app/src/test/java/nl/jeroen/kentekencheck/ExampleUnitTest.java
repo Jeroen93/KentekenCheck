c076b78a3271a795bc482dd5259de373fec3f255
Jeroen Aarts <j.aarts@quintor.nl>
2019-12-24 09:55:44 +0100

    Migrate tests to Junit5

--- app/src/test/java/nl/jeroen/kentekencheck/ExampleUnitTest.java
+++ app/src/test/java/nl/jeroen/kentekencheck/ExampleUnitTest.java
@@ -1,17 +1,19 @@
 package nl.jeroen.kentekencheck;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    void addition_isCorrect() {
+        assertThat(4, is(2 + 2));
     }
 }
--- app/src/test/java/nl/jeroen/kentekencheck/util/LicencePlateUtilTest.java
+++ app/src/test/java/nl/jeroen/kentekencheck/util/LicencePlateUtilTest.java
@@ -1,65 +1,63 @@
 package nl.jeroen.kentekencheck.util;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class LicencePlateUtilTest {
+class LicencePlateUtilTest {
 
-    @Test
-    public void getSidecodeLicensePlate_MultiplePlates_CheckCodes() {
-        testLicenceCode("invalid", -1);
-        testLicenceCode("99-XXX-99", -1);
-
-        testLicenceCode("CD-J-99", 0);
-
-        testLicenceCode("XX-99-99", 1);
-        testLicenceCode("99-99-XX", 2);
-        testLicenceCode("99-XX-99", 3);
-        testLicenceCode("XX-99-XX", 4);
-        testLicenceCode("XX-XX-99", 5);
-        testLicenceCode("99-XX-XX", 6);
-        testLicenceCode("99-XXX-9", 7);
-        testLicenceCode("9-XXX-99", 8);
-        testLicenceCode("XX-999-X", 9);
-        testLicenceCode("X-999-XX", 10);
-        testLicenceCode("XXX-99-X", 11);
-        testLicenceCode("X-99-XXX", 12);
-        testLicenceCode("9-XX-999", 13);
-        testLicenceCode("999-XX-9", 14);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void convertLicenseString_NullString_ThrowsIAException() {
-        LicencePlateUtil.convertLicenseString(null);
-    }
-
-    @Test
-    public void convertLicenseString_EmptyString_ReturnsEmptyString() {
-        String result = LicencePlateUtil.convertLicenseString("");
-
-        assertThat(result, is(emptyString()));
-    }
-
-    @Test
-    public void convertLicenseString_LowercaseString_ReturnsUppercaseString() {
-        String result = LicencePlateUtil.convertLicenseString("xx999X");
-
-        assertThat(result, is("XX999X"));
-    }
-
-    @Test
-    public void convertLicenseString_StringWithHyphen_ReturnsStringWithoutHyphen() {
-        String result = LicencePlateUtil.convertLicenseString("99-XX--99");
-
-        assertThat(result, is("99XX99"));
-    }
-
-    private void testLicenceCode(String licence, int expectedCode) {
-        int result = LicencePlateUtil.getSidecodeLicenseplate(licence);
+    @ParameterizedTest
+    @CsvSource({
+            "invalid,   -1",
+            "99-XXX-99, -1",
+            "CD-J99,    0",
+            "XX-99-99,  1",
+            "99-99-XX,  2",
+            "99-XX-99,  3",
+            "XX-99-XX,  4",
+            "XX-XX-99,  5",
+            "99-XX-XX,  6",
+            "99-XXX-9,  7",
+            "9-XXX-99,  8",
+            "XX-999-X,  9",
+            "X-999-XX,  10",
+            "XXX-99-X,  11",
+            "X-99-XXX,  12",
+            "9-XX-999,  13",
+            "999-XX-9,  14",
+    })
+    void getSidecodeLicensePlate_MultiplePlates_CheckCodes(String license, int expectedCode) {
+        int result = LicencePlateUtil.getSidecodeLicenseplate(license);
 
         assertThat(result, is(expectedCode));
+    }
+
+    @Test
+    void convertLicenseString_NullString_ThrowsIAException() {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() {
+                //noinspection ConstantConditions
+                LicencePlateUtil.convertLicenseString(null);
+            }
+        });
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', ''",
+            "xx999X, XX999X",
+            "99-XX--99, 99XX99"
+    })
+    void convertLicenseString_MultipleStrings(String rawLicense, String expedtedLicense) {
+        String result = LicencePlateUtil.convertLicenseString(rawLicense);
+
+        assertThat(result, is(expedtedLicense));
     }
 }
